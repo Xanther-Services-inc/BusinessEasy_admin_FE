c494807ca79b68bbf8c02052714eb42ffca491d4
imjoymhnt <mritunjoy.mahanta001@gmail.com>
2021-08-07 23:01:23 +0530

    order creation form add in the product details page

--- src/container/ecommerce/product/ProductDetails.js
+++ src/container/ecommerce/product/ProductDetails.js
@@ -12,32 +12,30 @@ const DetailsRight = lazy(() => import('./overview/DetailsRight'));
 const ProductDetails = ({ match }) => {
   const dispatch = useDispatch();
 
-  const productDetails = useSelector((state) => state.productDetail)
-  const {loading, data, error} = productDetails
-
+  const productDetails = useSelector(state => state.productDetail);
+  const { loading, data, error } = productDetails;
 
   useEffect(() => {
-    dispatch(productDetailsActionCreator(match.params.id))
-  }, [dispatch])
+    dispatch(productDetailsActionCreator(match.params.id));
+  }, [dispatch]);
 
   return (
     <>
-      
       <Main>
         <Cards headless>
           <ProductDetailsWrapper>
             <div className="product-details-box">
               <Row gutter={30}>
-                
-                <Col xs={24} lg={14}>
+                <Col xs={24} lg={14} md={24} xl={24}>
                   <Suspense
                     fallback={
                       <Cards headless>
                         <Skeleton active />
                       </Cards>
                     }
-                  > {loading ? 'Loading...' : data ? <DetailsRight product={data} /> : error }
-                    
+                  >
+                    {' '}
+                    {loading ? 'Loading...' : data ? <DetailsRight product={data} /> : error}
                   </Suspense>
                 </Col>
               </Row>
--- src/container/ecommerce/product/overview/DetailsRight.js
+++ src/container/ecommerce/product/overview/DetailsRight.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Row, Col, Image } from 'antd';
+import { Row, Col, Image, Form, Input, InputNumber, DatePicker, Select, Upload, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import Heading from '../../../../components/heading/heading';
 var _ = require('lodash');
@@ -12,34 +13,94 @@ const DetailsRight = ({ product }) => {
   const price = _.get(product, 'values.price');
   const desc = _.get(product, 'values.desc');
   const steps = _.get(product, 'values.steps');
+  const fields = _.get(product, 'values.users');
   const new_steps = steps.replace(/ /g, '').split(',');
-  console.log(new_steps);
+
+  console.log(fields);
 
   return (
-    <div className="product-details-box__right pdbr">
-      <Row>
-        <Col md={10} sm={24} lg={8} xl={8}>
-          <Image style={{ cursor: 'pointer' }} width={200} src={image} />
-        </Col>
-        <Col offset={2} />
-        <Col md={12} sm={24} lg={14} xl={14}>
-          <Heading className="pdbr__title" as="h2">
-            {title}
-          </Heading>
-          <Heading className="pdbr__new-price" as="h3">
-            <u className="pdbr__currency">Price:</u>
-            <span className="pdbr__price"> {price} Rs.</span>
-          </Heading>
+    <>
+      <div className="product-details-box__right pdbr">
+        <Row>
+          <Col md={10} sm={24} lg={8} xl={8}>
+            <Image style={{ cursor: 'pointer' }} width={400} src={image} />
+          </Col>
+          <Col offset={2} />
+          <Col md={12} sm={24} lg={14} xl={14}>
+            <Heading className="pdbr__title" as="h2">
+              {title}
+            </Heading>
+            <Heading className="pdbr__new-price" as="h3">
+              <u className="pdbr__currency">Price:</u>
+              <span className="pdbr__price"> {price} Rs.</span>
+            </Heading>
 
-          <p className="pdbr__desc">
-            <u>Product Id:</u> {id}
-          </p>
-          <p className="pdbr__desc">
-            <u>Description:</u> {desc}
-          </p>
-        </Col>
+            <p className="pdbr__desc">
+              <u>Product Id:</u> {id}
+            </p>
+            <p className="pdbr__desc">
+              <u>Description:</u> {desc}
+            </p>
+          </Col>
+        </Row>
+        <br />
+        <br />
+        <br />
+      </div>
+      <Heading className="pdbr__new-price" as="h3">
+        Order Creation Form
+      </Heading>
+      <br />
+      <Row gutter={30}>
+        {fields &&
+          fields.map(field => {
+            return field.formType === 'textInp' ? (
+              <Col md={8} xs={24}>
+                <Form.Item name={field.label} label={field.label}>
+                  <Input />
+                </Form.Item>
+              </Col>
+            ) : field.formType === 'imgField' ? (
+              <Col md={8} xs={24}>
+                <Upload>
+                  <label>{field.label}: </label>
+
+                  <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                </Upload>
+              </Col>
+            ) : field.formType === 'numberInp' ? (
+              <Col md={8} xs={24}>
+                <Form.Item name={field.label} label={field.label}>
+                  <InputNumber />
+                </Form.Item>
+              </Col>
+            ) : field.formType === 'select' ? (
+              <Col md={8} xs={24}>
+                <Form.Item name={field.label} label={field.label}>
+                  <Select>
+                    {console.log(field.options.split(','))}
+                    {field.options.split(',').map(opt => (
+                      <Option value={opt}>{opt}</Option>
+                    ))}
+                  </Select>
+                </Form.Item>
+              </Col>
+            ) : field.formType === 'date' ? (
+              <Col md={8} xs={24}>
+                <Form.Item name={field.label} label={field.label}>
+                  <DatePicker />
+                </Form.Item>
+              </Col>
+            ) : (
+              <Col md={8} xs={24}>
+                <Form.Item name={field.label} label={field.label}>
+                  <Input />
+                </Form.Item>
+              </Col>
+            );
+          })}
       </Row>
-    </div>
+    </>
   );
 };
 
