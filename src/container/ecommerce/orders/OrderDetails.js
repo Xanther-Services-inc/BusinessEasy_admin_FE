c8685d2c79cd91213707e924ba0019f6155fc574
subrata-bhunia <eerrcfgy@gmail.com>
2021-11-16 11:37:53 +0530

    Fix

--- src/container/ecommerce/orders/OrderDetails.js
+++ src/container/ecommerce/orders/OrderDetails.js
@@ -316,9 +316,10 @@ const ProjectDetails = ({ match }) => {
             <Cards title="Conversations">
               <Scrollbars
                 style={{
-                  width: '21rem',
+                  width: '17rem',
                   height: '20rem',
                   boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+                  borderRadius:'10px'
                 }}
               >
                 {messages &&
